4ad2872cb1f4b6a6feecd62a3fa8178f5cbc8a7e
Jonty800 <Jonty800@gmail.com>
2012-06-06 18:13:06 +0100

    update

--- MinecraftMemAllocate/MinecraftMemAllocate/Mainform.cs
+++ MinecraftMemAllocate/MinecraftMemAllocate/Mainform.cs
@@ -6,6 +6,8 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
 
 namespace MinecraftMemAllocate
 {
@@ -14,10 +16,28 @@ namespace MinecraftMemAllocate
         public string Path;
         public string Folder;
         public float Mem;
+        public int MemMin = 500;
+        public int MemMax = 1024;
+        string AppPath = System.IO.Path.Combine(Environment.GetFolderPath(
+        Environment.SpecialFolder.ApplicationData)) + "/MMA";
+        string ConfigFile = "MMAData.txt";
+        
         public Mainform()
         {
             InitializeComponent();
-            Mem = GetTotalMemoryInBytes() / 1048576000; //GB?
+            Mem = GetTotalMemoryInBytes() / 1048576;//ToMB
+            textBox5.Text = Mem.ToString() + "MB";
+            readAppConfig();
+            textBox3.Text = MemMin.ToString();
+            textBox4.Text = MemMax.ToString();
+        }
+
+        public void readAppConfig()
+        {
+            IEnumerable<string> data = File.ReadLines(AppPath + "/" + ConfigFile);
+            textBox1.Text = data.ElementAt(0);
+            textBox3.Text = data.ElementAt(1);
+            textBox4.Text = data.ElementAt(2);
         }
         public void FindFile()
         {
@@ -33,15 +53,6 @@ namespace MinecraftMemAllocate
                 textBox1.Text = fdlg.FileName;
             }
         }
-        public void FindFolder()
-        {
-            FolderBrowserDialog fdlg = new FolderBrowserDialog();
-            if (fdlg.ShowDialog() == DialogResult.OK)
-            {
-                Folder = fdlg.SelectedPath;
-                textBox2.Text = fdlg.SelectedPath;
-            }
-        }
 
         static ulong GetTotalMemoryInBytes()
         {
@@ -53,9 +64,41 @@ namespace MinecraftMemAllocate
             FindFile();
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void button3_Click(object sender, EventArgs e)
         {
-            FindFolder();
+            List<string> contents = new List<string>();
+            if (!Directory.Exists(AppPath)){
+                Directory.CreateDirectory(AppPath );
+            }
+            if (File.Exists(AppPath + "/" + ConfigFile))
+            {
+                File.Delete(AppPath + "/" + ConfigFile);
+            }
+            contents.Add(Path);
+            contents.Add(MemMin.ToString());
+            contents.Add(MemMax.ToString());
+            File.WriteAllLines(AppPath+"/"+ConfigFile, contents);
+            OpenCmdWithCmds();
+        }
+
+        public void OpenCmdWithCmds()
+        {
+            Process process = new Process();
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.FileName = "cmd.exe";
+            startInfo.Arguments = "/C java " + "-Xms" + MemMin + "m" + " -Xmx" + MemMax + "m -jar " + "\""+Path+"\"";
+            process.StartInfo = startInfo;
+            process.Start();
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            MemMin = int.Parse(this.textBox3.Text);
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            MemMin = int.Parse(this.textBox4.Text);
         }
     }
     
