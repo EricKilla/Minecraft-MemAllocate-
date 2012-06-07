582a410d3c8a6d165b201697d289a004f30f4273
Jonty800 <Jonty800@gmail.com>
2012-06-07 12:14:09 +0100

    Release

--- MinecraftMemAllocate/MinecraftMemAllocate/Mainform.Designer.cs
+++ MinecraftMemAllocate/MinecraftMemAllocate/Mainform.Designer.cs
@@ -28,10 +28,10 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Mainform));
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.textBox4 = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
@@ -40,12 +40,13 @@
             this.button3 = new System.Windows.Forms.Button();
             this.textBox5 = new System.Windows.Forms.TextBox();
             this.label7 = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(289, 57);
+            this.button1.Location = new System.Drawing.Point(289, 41);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -56,7 +57,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(25, 62);
+            this.label1.Location = new System.Drawing.Point(25, 51);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(114, 13);
             this.label1.TabIndex = 1;
@@ -64,23 +65,15 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(145, 59);
+            this.textBox1.Location = new System.Drawing.Point(145, 44);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(138, 20);
             this.textBox1.TabIndex = 3;
             // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(145, 126);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(84, 20);
-            this.textBox3.TabIndex = 7;
-            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
-            // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(48, 126);
+            this.label3.Location = new System.Drawing.Point(48, 86);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(91, 13);
             this.label3.TabIndex = 6;
@@ -88,7 +81,7 @@
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(145, 158);
+            this.textBox4.Location = new System.Drawing.Point(145, 112);
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(84, 20);
             this.textBox4.TabIndex = 9;
@@ -97,7 +90,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(48, 158);
+            this.label4.Location = new System.Drawing.Point(45, 115);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(94, 13);
             this.label4.TabIndex = 8;
@@ -106,7 +99,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(235, 129);
+            this.label5.Location = new System.Drawing.Point(235, 86);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(23, 13);
             this.label5.TabIndex = 10;
@@ -115,7 +108,7 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(235, 161);
+            this.label6.Location = new System.Drawing.Point(235, 115);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(23, 13);
             this.label6.TabIndex = 11;
@@ -123,18 +116,18 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(289, 191);
+            this.button3.Location = new System.Drawing.Point(271, 151);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.Size = new System.Drawing.Size(93, 23);
             this.button3.TabIndex = 12;
-            this.button3.Text = "Create";
+            this.button3.Text = "Start Minecraft";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // textBox5
             // 
             this.textBox5.Enabled = false;
-            this.textBox5.Location = new System.Drawing.Point(226, 12);
+            this.textBox5.Location = new System.Drawing.Point(286, 6);
             this.textBox5.Name = "textBox5";
             this.textBox5.Size = new System.Drawing.Size(78, 20);
             this.textBox5.TabIndex = 14;
@@ -142,27 +135,40 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(149, 15);
+            this.label7.Location = new System.Drawing.Point(203, 9);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(71, 13);
+            this.label7.Size = new System.Drawing.Size(80, 13);
             this.label7.TabIndex = 13;
-            this.label7.Text = "System RAM:";
+            this.label7.Text = "Available RAM:";
             // 
-            // label8
+            // textBox3
             // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(310, 15);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(23, 13);
-            this.label8.TabIndex = 15;
-            this.label8.Text = "MB";
+            this.textBox3.Location = new System.Drawing.Point(145, 83);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(84, 20);
+            this.textBox3.TabIndex = 7;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(58, 138);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(186, 39);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Did nothing happen? \r\nTry setting your RAM to a much \r\nlower number, like 1024mb " +
+    "or 2048mb";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label2.Visible = false;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // Mainform
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(376, 228);
-            this.Controls.Add(this.label8);
+            this.ClientSize = new System.Drawing.Size(376, 186);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox5);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.button3);
@@ -175,6 +181,7 @@
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Mainform";
             this.Text = "Minecraft MemAllocate";
             this.ResumeLayout(false);
@@ -187,7 +194,6 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox textBox4;
         private System.Windows.Forms.Label label4;
@@ -196,7 +202,8 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.TextBox textBox5;
         private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- MinecraftMemAllocate/MinecraftMemAllocate/Mainform.cs
+++ MinecraftMemAllocate/MinecraftMemAllocate/Mainform.cs
@@ -1,45 +1,131 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Windows.Forms;
-using System.IO;
 using System.Diagnostics;
+using System.IO;
+using System.Xml;
+using System.Xml.Linq;
+using Microsoft.VisualBasic.Devices;
 
 namespace MinecraftMemAllocate
 {
     public partial class Mainform : Form
     {
-        public string Path;
-        public string Folder;
-        public float Mem;
-        public int MemMin = 500;
-        public int MemMax = 1024;
+        string Path;
+        float Mem;
+        int MemMin = 500;
+        int MemMax = 1024;
+        object xmlLock = new object();
         string AppPath = System.IO.Path.Combine(Environment.GetFolderPath(
         Environment.SpecialFolder.ApplicationData)) + "/MMA";
-        string ConfigFile = "MMAData.txt";
-        
+        string ConfigFile = "Config.xml";
+        string javadir;
+
         public Mainform()
         {
             InitializeComponent();
             Mem = GetTotalMemoryInBytes() / 1048576;//ToMB
             textBox5.Text = Mem.ToString() + "MB";
             readAppConfig();
-            textBox3.Text = MemMin.ToString();
-            textBox4.Text = MemMax.ToString();
+            LocateJava();
+        }
+
+        void LocateJava()
+        {
+            String path = Environment.GetEnvironmentVariable("path");
+            String[] folders = path.Split(';');
+            foreach (String folder in folders)
+            {
+                if (File.Exists(folder + "java.exe"))
+                {
+                    this.javadir = folder + "/java.exe";
+                    return;
+                }
+                else if (File.Exists(folder + "\\java.exe"))
+                {
+                    this.javadir = folder + "/java.exe";
+                    return;
+                }
+            }
+            if (javadir == null)
+            {
+                MessageBox.Show("Unable to find your Java.exe (Not good)");
+            }
+        }
+
+        bool _myTextBoxChanging = false;
+        void validateText(TextBox box)
+        {
+            // stop multiple changes;
+            if (_myTextBoxChanging)
+                return;
+            _myTextBoxChanging = true;
+
+            string text = box.Text;
+            if(box.Text.Length == 0)
+                return;
+            string validText = "";
+            int pos = box.SelectionStart;
+            for (int i = 0; i < text.Length; i++)
+            {
+                bool badChar = false;
+                char s = text[i];
+                if (s == '.')
+                {
+                    badChar = true;
+                }
+                else if (s < '0' || s > '9')
+                    badChar = true;
+
+                if (!badChar)
+                    validText += s;
+                else
+                {
+                    if (i <= pos)
+                        pos--;
+                }
+            }
+            while (validText.Length >= 2 && validText[0] == '0')
+            {
+                if (validText[1] != '.')
+                {
+                    validText = validText.Substring(1);
+                    if (pos < 2)
+                        pos--;
+                }
+                else
+                    break;
+            }
+
+            if (pos > validText.Length)
+                pos = validText.Length;
+            box.Text = validText;
+            box.SelectionStart = pos;
+            _myTextBoxChanging = false;
         }
 
         public void readAppConfig()
         {
-            IEnumerable<string> data = File.ReadLines(AppPath + "/" + ConfigFile);
-            textBox1.Text = data.ElementAt(0);
-            textBox3.Text = data.ElementAt(1);
-            textBox4.Text = data.ElementAt(2);
+            lock (xmlLock)
+            {
+                if (File.Exists(AppPath + "/" + ConfigFile))
+                {
+                    XmlTextReader xmlReader = new XmlTextReader(AppPath + "/" + ConfigFile);
+                    while (xmlReader.Read())
+                    {
+                        if (xmlReader.Name == "GeneralSettings")
+                        {
+                            Path = xmlReader.GetAttribute("Path").ToString(); textBox1.Text = Path;
+                            MemMin = Int32.Parse(xmlReader.GetAttribute("MemMin")); textBox3.Text = MemMin.ToString();
+                            MemMax = Int32.Parse(xmlReader.GetAttribute("MemMax")); textBox4.Text = MemMax.ToString();
+                        }
+                    }
+                    xmlReader.Close();
+                }
+            }
         }
-        public void FindFile()
+
+        void FindFile()
         {
             OpenFileDialog fdlg = new OpenFileDialog();
             fdlg.Title = "Minecraft.exe finder";
@@ -56,51 +142,105 @@ namespace MinecraftMemAllocate
 
         static ulong GetTotalMemoryInBytes()
         {
-            return new Microsoft.VisualBasic.Devices.ComputerInfo().TotalPhysicalMemory;
+            return new ComputerInfo().AvailablePhysicalMemory;
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        void button1_Click(object sender, EventArgs e)
         {
             FindFile();
         }
 
-        private void button3_Click(object sender, EventArgs e)
+        void button3_Click(object sender, EventArgs e)
         {
-            List<string> contents = new List<string>();
-            if (!Directory.Exists(AppPath)){
-                Directory.CreateDirectory(AppPath );
-            }
-            if (File.Exists(AppPath + "/" + ConfigFile))
+            SaveConfig();
+            double Num;
+            bool isNum = double.TryParse(textBox3.Text.Trim(), out Num);
+            if (!isNum)
             {
-                File.Delete(AppPath + "/" + ConfigFile);
+                MessageBox.Show("Invalid number at Minimum Memory");
+                return;
             }
-            contents.Add(Path);
-            contents.Add(MemMin.ToString());
-            contents.Add(MemMax.ToString());
-            File.WriteAllLines(AppPath+"/"+ConfigFile, contents);
+            isNum = double.TryParse(textBox4.Text.Trim(), out Num);
+            if (!isNum)
+            {
+                MessageBox.Show("Invalid number at Maximum Memory");
+                return;
+            }
+            if (MemMin < 10)
+            {
+                MessageBox.Show("Minimum Memory cannot be less than 10mb");
+                return;
+            }
+            if (MemMin > MemMax)
+            {
+                MessageBox.Show("Minimum Memory cannot exceed the maximum");
+                return;
+            }
+            if (MemMax > Mem || MemMin > Mem)
+            {
+                MessageBox.Show("Memory values cannot exceed available RAM");
+                return;
+            }
             OpenCmdWithCmds();
+            label2.Visible = true;
         }
 
-        public void OpenCmdWithCmds()
+        void SaveConfig()
+        {
+            lock (xmlLock)
+            {
+                if (!Directory.Exists(AppPath))
+                {
+                    Directory.CreateDirectory(AppPath);
+                }
+                XmlWriterSettings settings = new XmlWriterSettings();
+                settings.Indent = true;
+                XmlWriter writer = XmlWriter.Create(AppPath + "/" + ConfigFile, settings);
+                writer.WriteStartDocument();
+                writer.WriteComment("This file is generated by the program.");
+                writer.WriteStartElement("GeneralSettings");
+                writer.WriteAttributeString("Path", Path);
+                writer.WriteAttributeString("MemMin", MemMin.ToString());
+                writer.WriteAttributeString("MemMax", MemMax.ToString());
+                writer.WriteEndElement();
+                writer.WriteEndDocument();
+                writer.Flush();
+                writer.Close();
+            }
+        }
+
+        void OpenCmdWithCmds()
         {
             Process process = new Process();
             ProcessStartInfo startInfo = new ProcessStartInfo();
             startInfo.FileName = "cmd.exe";
-            startInfo.Arguments = "/C java " + "-Xms" + MemMin + "m" + " -Xmx" + MemMax + "m -jar " + "\""+Path+"\"";
+            startInfo.Arguments = "/C " + javadir + " " + " -Xmx" + MemMax + "m -Xms" + MemMin + "m" + " -jar " + "\"" + Path + "\"";
+            //MessageBox.Show(startInfo.Arguments);
             process.StartInfo = startInfo;
             process.Start();
         }
 
-        private void textBox3_TextChanged(object sender, EventArgs e)
+        void textBox3_TextChanged(object sender, EventArgs e)
         {
-            MemMin = int.Parse(this.textBox3.Text);
+            if (textBox3.TextLength > 0)
+            {
+                validateText(textBox3);
+                MemMin = int.Parse(this.textBox3.Text);
+            }
         }
 
-        private void textBox4_TextChanged(object sender, EventArgs e)
+        void textBox4_TextChanged(object sender, EventArgs e)
         {
-            MemMin = int.Parse(this.textBox4.Text);
+            if (textBox3.TextLength > 0)
+            {
+                validateText(textBox4);
+                MemMax = int.Parse(this.textBox4.Text);
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
         }
     }
-    
-
 }
